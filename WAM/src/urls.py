50ad387d9379a86b07b4f255830770a534bb7ec9
Andrew Graham <agraham3@cougars.ccis.edu>
2015-02-15 02:05:37 -0600

    Pushing a temporary set up for the tic-tac-toe demo for Tuesday!

--- WAM/src/urls.py
+++ WAM/src/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
