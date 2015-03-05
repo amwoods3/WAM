a949af84ac72ec169b48ef27b77cb7c7df37f2a9
Andrew Graham <agraham3@cougars.ccis.edu>
2015-03-05 10:54:09 -0600

    Fixed the problem of django not finding the javascript function with child inheritance.

--- wam/static/hello_world.js
+++ wam/static/hello_world.js
@@ -1,5 +1,4 @@
 function entryPoint() {
-    alert('entryPoint() entry point.');
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
 
@@ -7,7 +6,5 @@ function entryPoint() {
     context.font = '40pt Calibri';
     context.fillStyle = 'blue';
     context.fillText('Hello World!', 150, 100);
-
-    alert('entryPoint() returning.');
 }
 
--- wam/wam/settings.py
+++ wam/wam/settings.py
@@ -84,4 +84,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
