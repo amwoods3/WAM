15dc95bee7ad0d20bf4c03eef9ab3b2c710a363f
amwoods3 <amwoods3@cougars.ccis.edu>
2015-03-04 19:49:38 -0600

    Updating the index file.

---
+++ wam/game/templates/game/hello_world.js
@@ -0,0 +1,14 @@
+function entryPoint() {
+    alert('entryPoint() entry point.');
+`
+    var canvas = document.getElementById('myCanvas');
+    var context = canvas.getContext('2d');
+
+    // do cool things with the context
+    context.font = '40pt Calibri';
+    context.fillStyle = 'blue';
+    context.fillText('Hello World!', 150, 100);
+
+    alert('entryPoint() returning.');
+}
+
