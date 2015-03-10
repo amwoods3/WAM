6de091e3b902dc277b23b4f20343406b57e4d4fe
Andrew Graham <agraham3@cougars.ccis.edu>
2015-03-10 12:13:49 -0500

    Fixed more bugs in url pathing

--- wam/game/views.py
+++ wam/game/views.py
@@ -9,7 +9,7 @@ from config import FILE_PATH
 
 # f is file object, n is user_name/title_of_file
 def handle_uploaded_file(f, n):
-    with open(FILE_PATH + 'wam/ais/' + n + '.py', 'wb+') as destination:
+    with open(FILE_PATH + '/wam/ais/' + n + '.py', 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
             
@@ -97,7 +97,7 @@ def register(request):
 
             # registeration successful
             import os
-            os.system('mkdir %swam/ais/' % (FILE_PATH) + post['user_name'])
+            os.system('mkdir %s/wam/ais/' % (FILE_PATH) + post['user_name'])
             user = UserLogin(user_name=post['user_name'], password=post['password'], email=post['email'])
             user.save()
             return HttpResponseRedirect('/game/login')
