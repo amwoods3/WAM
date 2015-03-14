f15dddc716ccb03ee60cf2f121d74e8b1342802f
Andrew Graham <agraham3@cougars.ccis.edu>
2015-03-13 20:01:21 -0500

    Changed the html code of challenge user and play Changed the url flow of the website Added choose game type to challege user page

--- wam/game/play.py
+++ wam/game/play.py
@@ -7,7 +7,7 @@ from game.views import logged_in
 from game.models import UserLogin, UserAiTable, UserStats, PastGames
 from config import FILE_PATH
 
-def challenge_users_ai(request):
+def challenge_user(request):
     try:
         del request.session['played']
     except:
@@ -21,15 +21,21 @@ def challenge_users_ai(request):
         return HttpResponseRedirect('/game')
     
     if request.method == 'POST':
+        # get the challenged user's username
         user_id = UserLogin.objects.get(user_name=request.POST['ch_user'])
         request.session['challenged_user'] = user_id.id
-        return HttpResponseRedirect('/game/view_user')
+
+        # Ai V Ai
+        if request.POST['game_type'] == 'AivAi':
+            return HttpResponseRedirect('/game/view_user_ai')
+        else:
+            c['error_message'] = 'Sorry the game mode %s is not available yet.' % request.POST['game_type']
     
     users = UserLogin.objects.all()
     users = [x.user_name for x in users if UserAiTable.objects.all().filter(user_id=x.id)]
     c['can_challenge'] = users
     c.update(csrf(request))
-    return render(request, 'game/challenge_users_ai.html', c)
+    return render(request, 'game/challenge_user.html', c)
 
 def view_user_ai(request):
     c = {'user_logged_in': logged_in(request)}
@@ -61,7 +67,7 @@ def view_user_ai(request):
     user = UserLogin.objects.get(pk=request.session['challenged_user'])
     c['ch_user_name'] = user.user_name
     c.update(csrf(request))
-    return render(request, 'game/view_user.html', c)
+    return render(request, 'game/view_user_ai.html', c)
 
 def play(request):
     c = {'user_logged_in': logged_in(request)}
