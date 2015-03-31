b87b1d62e1a8fa49e41dfb2f69c60755503da24c
amwoods3 <amwoods3@cougars.ccis.edu>
2015-03-31 05:14:22 -0500

    Check communicate

--- games/run_ai.py
+++ games/run_ai.py
@@ -1,3 +1,3 @@
 import v2_rand as ai
-print ai.get_move('[[" ", "x", " "], ["o", "x", "o"], ["x", " ", "o"]]', 942.0, 'x')
+print ai.get_move('[["x", "o", " "], ["o", "x", "x"], ["x", "o", "o"]]', 922.0, 'x')
     
--- games/time_ai.py
+++ games/time_ai.py
@@ -4,24 +4,26 @@ import json
 def set_limit(seconds):
     soft, hard = resource.getrlimit(resource.RLIMIT_CPU)
     resource.setrlimit(resource.RLIMIT_CPU,
-                       (seconds, hard))
+                       (seconds, soft))
 
 def run_ai(ai, board, time_limit, turn):
     cmd = "python run_ai.py"
+    
     file("run_ai.py", 'w').write("""import %s as ai
 print ai.get_move('%s', %s, '%s')
     """ % (ai, json.JSONEncoder().encode(board), str(time_limit), turn))
+    
     t0 = resource.getrusage(resource.RUSAGE_CHILDREN)
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE, preexec_fn= lambda : set_limit(time_limit/1000.0))
     p.wait()
     t1 = resource.getrusage(resource.RUSAGE_CHILDREN)
-    time_taken = ((t1.ru_utime + t1.ru_stime) - (t0.ru_utime + t0.ru_stime)) * 1000
-    print "time_limit:", time_limit
-    print "time_taken:", time_taken
+    
+    time_taken = ((t1.ru_utime + t1.ru_stime) - (t0.ru_utime + t0.ru_stime)) * 1000 # need time_taken in miliseconds, we want to use for ai records
     try:
+        print p.communicate()[0]
         result = json.JSONDecoder().decode(p.communicate()[0])
-    except ValueError:
+    except ValueError: # will happen if AI doesn't have enough time to return value
         print "Time out!!"
         result = [9999, 9999]
     return (result, time_taken)
