607b876a693f1463eed60d2fcef1882ec6fed047
amwoods3 <amwoods3@cougars.ccis.edu>
2015-04-07 10:10:33 -0500

    Removed old check that is no longer necessary.

--- games/boardgames.py
+++ games/boardgames.py
@@ -1,3 +1,92 @@
+class TicTacToe:
+    def __init__(self, n=3, s='', history=[]):
+        self.state = []
+        if s != '':
+            s = s.split(',')
+        self.n = n
+        index = 0
+        for i in range(n):
+            k = []
+            for j in range(n):
+                if s != '' and index < len(s):
+                    k.append(s[index])
+                else:
+                    k.append(' ')
+                index += 1
+            self.state.append(k)
+        if len(history) > 0:
+            for move in history:
+                self.state[move[1]][move[2]] = move[0]
+    def __str__(self):
+        s = ''
+        for i in range(self.n):
+            if i != 0 and i != self.n:
+                for j in range(self.n):
+                    s += '-'
+                    if j != self.n - 1:
+                        s += '+'
+                s += '\n'
+            for j in range(self.n):
+                s += self.state[i][j]
+                if j != self.n - 1:
+                    s += '|'
+            s += '\n'
+        return s
+    def get_state_str(self):
+        s = ''
+        for row in self.state:
+            for col in row:
+                s += col + ','
+        return s[:-1]
+    def insert(self, piece, r, c):
+        if type(r) is not int or type(c) is not int:
+            return False
+        if r >= self.n or c >= self.n:
+            return False
+        if self.state[r][c] != ' ':
+            return False
+        self.state[r][c] = piece
+        return True
+    def match(self, line, piece):
+        c = piece
+        win = [c == cp for cp in line]
+        if False in win:
+            return False
+        return True
+    def build_row(self, r):
+        return list(self.state[r])
+    def build_col(self, c):
+        col = []
+        for row in self.state:
+            col.append(row[c])
+        return col
+    def build_down_diag(self):
+        diag = []
+        for r in range(self.n):
+            diag.append(self.state[r][r])
+        return diag
+    def build_up_diag(self):
+        diag = []
+        for r in range(self.n):
+            diag.append(self.state[r][self.n - 1 - r])
+        return diag
+    def check_win(self, piece):
+        for i in range(self.n):
+            if self.match(self.build_row(i), piece) or \
+                   self.match(self.build_col(i), piece):
+                return True
+        if self.match(self.build_down_diag(), piece) or \
+               self.match(self.build_up_diag(), piece):
+            return True
+        return False
+    def full(self):
+        for row in self.state:
+            for col in row:
+                if col == ' ':
+                    return False
+        return True
+
+
 class GameBoard(object):
     def __init__(self, n, s='', m=None):
         self.state = []
@@ -80,9 +169,9 @@ class GameController:
 
     
     def manage_turn(self, ttt, ai=['','']):
-        if type(ttt) != type(TicTacToe()):
+        #if type(ttt) != type(TicTacToe()):
             #throw some error
-            return False
+            #return False
         
         if ai[self.player]['ai'] == '':
             r, c = self.get_input()
