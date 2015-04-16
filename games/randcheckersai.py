c4efc1adeac5f948dd190df8f4117653ed523962
amwoods3 <amwoods3@cougars.ccis.edu>
2015-04-16 00:30:20 -0500

    Ai for checkers added. May not be the best, but let's test it.

--- games/randcheckersai.py
+++ games/randcheckersai.py
@@ -12,6 +12,7 @@ def get_move(state, time_limit, turn):
     direction = 1 if turn == 'b' else -1
     enemy = ('r', 'R') if turn == 'b' else ('b', 'B')
     # check for possible jumps
+    jumps = []
     for p in pos:
         r = p[0], c = p[1]
         r += direction
@@ -19,7 +20,26 @@ def get_move(state, time_limit, turn):
         c1 = c + 1
         c2 = c - 1
         if state[r][c1] in enemy:
-            c2 = c1 + 1
-            if state[r1][c1] == ' ':
-                pass
-            
+            c3 = c1 + 1
+            if r1 < 8 and c3 < 8 and r1 >= 0:
+                if state[r1][c3] == ' ':
+                    jumps.append((p[0], p[1], r1, c3))
+        if state[r][c2] in enemy:
+            c3 = c2 - 1
+            if r1 < 8 and c3 >= 0 and r1 >= 0:
+                if state[r1][c3] == ' ':
+                    jumps.append((p[0], p[1], r1, c3))
+    if len(jumps) > 0:
+        return json.dumps([random.choice(jumps)])
+    moves = []
+    for p in pos:
+        r = p[0], c = p[1]
+        r += direction
+        c1 = c + 1
+        c2 = c - 1
+        if r < 8 and r >= 0 and c1 < 8 and c2 >= 0:
+            if state[r][c1] == ' ':
+                moves.append((p[0], p[1], r, c1))
+            if state[r][c2] == ' ':
+                moves.append((p[0], p[1], r, c2))
+    return json.dumps([random.choice(moves)])
