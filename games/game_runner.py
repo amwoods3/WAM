c63c7339968e637e76a7ef1a8dbfb92656533dbb
amwoods3 <amwoods3@cougars.ccis.edu>
2015-04-15 20:01:13 -0500

    Write to database each turn. Does it do it properly? I don't know...

--- games/game_runner.py
+++ games/game_runner.py
@@ -1,6 +1,7 @@
 from boardgames import *
 from checkers import *
-
+from games.models import ActiveGame
+import json
 def something(s,move):
     exec(s)
     move[0] = r
@@ -13,7 +14,7 @@ def something(s,move):
 
 
 def play_game(users, ais, hist=None, turns=-1,time=0,p1time=0,p2time=0,
-              game_type='tic_tac_toe'):
+              game_type='tic_tac_toe', p1id=1, p2id=2):
     if hist is None:
         hist = []
     ai = [{'user' : users[0], 'ai': ais[0]}, {'user' : users[1], 'ai' : ais[1]}]
@@ -25,9 +26,22 @@ def play_game(users, ais, hist=None, turns=-1,time=0,p1time=0,p2time=0,
     elif game_type == 'checkers':
         game = Checkers(history=hist)
         controller = GameController(history=hist, time=time, p1t=p1time,p2t=p2time, player1='b', player2='r')
+
+        # create a point in database for this game.
+    cur_game = ActiveGame(game_state=json.dumps(game.state), last_move= '', player1_id=p1id, player2_id=p2id, player1_timer=0, player2_timer=0, is_player1_turn = True, player1_ai=ais[0], player2_ai=ais[1])
+    cur_game.save()
+
+    
     while turns is not 0:
         if controller.manage_turn(game, ai) == 0:
             break
+        else:
+            cur_game.game_state = json.dumps(game.state)
+            cur_game.last_move = json.dumps(controller.history[-1])
+            cur_game.player1_timer=controller.timers[0]
+            cur_game.player2_timer=controller.timers[1]
+            cur_game.is_player1_turn = controller.player == 0
+            cur_game.save()
         if controller.get_winner() != ' ':
             break
         if turns is not -1:
