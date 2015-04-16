b36c5b133517e2ef6450581f0b71b5454eb68640
amwoods3 <amwoods3@cougars.ccis.edu>
2015-04-15 22:37:32 -0500

    Database commented out for now.

--- games/game_runner.py
+++ games/game_runner.py
@@ -1,6 +1,6 @@
 from boardgames import *
 from checkers import *
-from wam.game.models import ActiveGame
+#from wam.game.models import ActiveGame
 import json
 def something(s,move):
     exec(s)
@@ -28,20 +28,20 @@ def play_game(users, ais, hist=None, turns=-1,time=0,p1time=0,p2time=0,
         controller = GameController(history=hist, time=time, p1t=p1time,p2t=p2time, player1='b', player2='r')
 
         # create a point in database for this game.
-    cur_game = ActiveGame(game_state=json.dumps(game.state), last_move= '', player1_id=p1id, player2_id=p2id, player1_timer=0, player2_timer=0, is_player1_turn = True, player1_ai=ais[0], player2_ai=ais[1], game_type=game_type)
-    cur_game.save()
+    #cur_game = ActiveGame(game_state=json.dumps(game.state), last_move= '', player1_id=p1id, player2_id=p2id, player1_timer=0, player2_timer=0, is_player1_turn = True, player1_ai=ais[0], player2_ai=ais[1], game_type=game_type)
+    #cur_game.save()
 
     
     while turns is not 0:
         if controller.manage_turn(game, ai) == 0:
             break
-        else:
-            cur_game.game_state = json.dumps(game.state)
-            cur_game.last_move = json.dumps(controller.history[-1])
-            cur_game.player1_timer=controller.timers[0]
-            cur_game.player2_timer=controller.timers[1]
-            cur_game.is_player1_turn = controller.player == 0
-            cur_game.save()
+        #else:
+        #    cur_game.game_state = json.dumps(game.state)
+        #    cur_game.last_move = json.dumps(controller.history[-1])
+        #    cur_game.player1_timer=controller.timers[0]
+        #    cur_game.player2_timer=controller.timers[1]
+        #    cur_game.is_player1_turn = controller.player == 0
+        #    cur_game.save()
         if controller.get_winner() != ' ':
             break
         if turns is not -1:
